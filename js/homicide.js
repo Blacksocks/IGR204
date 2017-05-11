763548686b3511c1aa1796c4aa9214ba77090971
Blacksocks <20.100gaillard@gmail.com>
2017-05-11 04:40:58 +0200

    set informations, colors, mouseover, names

--- js/homicide.js
+++ js/homicide.js
@@ -1,40 +1,62 @@
-var states = ["", "Alabama", "Alaska", "", "Arizona", "Arkansas", "California", "", "Colorado", "Connecticut", "Delaware", "",
-    "Florida", "Georgia", "", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
+var statesNames = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
+    "Washington_", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
     "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
     "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
     "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
-    "Pennsylvania", "", "Rhodes Island", "South Carolina", "South Dakota", "Tennessee", "Texas",
-    "Utah", "Vermont", "Virginia", "", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+    "Pennsylvania", "Rhodes Island", "South Carolina", "South Dakota", "Tennessee", "Texas",
+    "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
 
 var months = ["January", "January", "February", "March", "April", "May",
     "June", "July", "August", "September", "October", "November", "December"]
 
-var dataset;
-var width = 960;
-var height = 600;
-var svg = d3.select('svg')
-	.attr('width', width)
-	.attr('height', height);
-var statesData = [];
-for(var i = 0; i < states.length; i++)
-    statesData[i] = 0;
-var statesDataMin = 0;
-var statesDataMax = 0;
-var path = d3.geoPath();
+function State(name) {
+    this.name = name;
+    this.id = 0;
+    this.death = 0;  // number of death (men + women)
+    this.men = 0;    // number of death (men)
+    // Black, White, Native American, Asian
+    this.ethnic = [0, 0, 0, 0];
+    this.color = "#000000";
+}
 
-d3.csv("homicides.csv", function(data) {
-    dataset = data.map(function(d) {
+// map position and size
+var ratio = 1.67; // width on height country ratio
+var width = $(window).width() * 0.8;
+var height = width / ratio;
+if($(window).width() / $(window).height() > ratio) {
+    height = $(window).height() * 0.8;
+    width = height * ratio;
+}
+$("#map").css("top", (($(window).height() - height) / 2) + "px");
+$("#map").css("left", (($(window).width() - width) / 2) + "px");
+var svg = d3.select('svg')
+    .style("width", width + 'px')
+    .style("height", height + 'px');
+// init states data
+var statesData = [];
+for(var i = 0; i < statesNames.length; i++)
+    statesData[i] = new State(statesNames[i]);
+minDeath = 0;
+maxDeath = 0;
+var path = d3.geoPath();
+// convert state id to statesNames array index
+idlnk = []
+// convert statesNames array index into state id
+idlnk_ = []
+
+d3.csv("data/homicides_small.csv", function(data) {
+    data.map(function(d) {
         // for each homicide
-        stateIdx = states.indexOf(d["State"]);
+        stateIdx = statesNames.indexOf(d["State"]);
         if(stateIdx == -1) {
             // Handle case "District of Columbia" because it is also "Washingtown"
-            if(d["State"] == "District of Columbia") stateIdx = 40;
+            if(d["State"] == "District of Columbia") stateIdx = 46;
             else {
                 console.log("[ERROR] State not found: " + d["State"]);
                 return;
             }
         }
-        infos = [+d["Year"],
+        /*infos = [+d["Year"],
                months.indexOf(d["Month"]) + 1,
               +d["Incident"],
                d["Crime Type"],
@@ -46,22 +68,55 @@ d3.csv("homicides.csv", function(data) {
               +d["Perpetrator Age"],
                d["Perpetrator Race"],
                d["Relationship"],
-               d["Weapon"]];
-        statesData[stateIdx]++;
-        return stateIdx + infos;
+               d["Weapon"]];*/
+        statesData[stateIdx].death++;
+        if(d["Victim Sex"] == "Male") statesData[stateIdx].men++;
+        if(d["Victim Race"] == "Black")
+            statesData[stateIdx].ethnic[0]++;
+        else if(d["Victim Race"] == "White")
+            statesData[stateIdx].ethnic[1]++;
+        else if(d["Victim Race"] == "Native American/Alaska Native")
+            statesData[stateIdx].ethnic[2]++;
+        else if(d["Victim Race"] == "Asian/Pacific Islander")
+            statesData[stateIdx].ethnic[3]++;
+        else
+            console.log("[WARNING] Ethnie not found: " + d["Victim Race"]);
     });
     //update min and max
-    statesDataMin = d3.min(statesData);
-    statesDataMax = d3.max(statesData);
+    minDeath = statesData[0].death;
+    maxDeath = minDeath;
+    for(var i = 1; i < statesNames.length; i++) {
+        if(statesData[i].death < minDeath) minDeath = statesData[i].death;
+        else if(statesData[i].death > maxDeath) maxDeath = statesData[i].death;
+    }
     // load map
     d3.queue()
         .defer(d3.json, "https://d3js.org/us-10m.v1.json")
         .await(ready);
 });
 
+function c(x) {
+    return Math.sqrt(x);
+}
+
 function getColor(id) {
-    val = Math.round((statesData[id] - statesDataMin) * 255 / (statesDataMax - statesDataMin));
-    return (val < 16 ? "0" : "") + val.toString(16);
+    min = 50;
+    max = 230;
+    val = Math.round((c(statesData[id].death) - c(minDeath)) * (max - min) / (c(maxDeath) - c(minDeath))) + min;
+    val =  256 - val; // invert
+    val = (val < 16 ? "0" : "") + val.toString(16);
+    return "#ee" + val + val;
+}
+
+function hideAllStatesBut(id) {
+    for(var i = 0; i < statesData.length; i++)
+        if(i != idlnk[id])
+            $("#state" + idlnk_[i]).attr("fill", "rgba(0, 0, 0, 0)");
+}
+
+function showAllStates() {
+    for(var i = 0; i < statesData.length; i++)
+        $("#state" + idlnk_[i]).attr("fill", statesData[i].color);
 }
 
 function ready(error, us) {
@@ -72,11 +127,50 @@ function ready(error, us) {
         .data(topojson.feature(us, us.objects.states).features)
         // for each state
         .enter().append("path")
-        // state color
-        .attr("fill", function(d) {c = getColor(parseInt(d.id)); return "#"+c+c+c;})
+        .attr("id", function(d) {idlnk_.push(parseInt(d.id));return "state" + parseInt(d.id);})
         .attr("d", path)
-        // on mouse over event
+        .attr("stroke", function(d) {return "#eeeeee";})
+        .attr("transform", "scale(" + width / 1000 + ")")
+        // on mouse event
         .on("mouseover", function(d){
-            return document.getElementById("name").innerHTML = d.id + " - " + states[parseInt(d.id)];
+            id = idlnk[parseInt(d.id)];
+            hideAllStatesBut(parseInt(d.id));
+            document.getElementById("name").innerHTML = id + " (" + parseInt(d.id) + ")" + " - " + statesData[id].name + ", death:" + statesData[id].death;
+        })
+        .on("mouseleave", function(d){
+            showAllStates();
         });
+    // manage country index
+    idlnk_.sort(function(a, b) {return a - b;});
+    idlnk.length = idlnk_[idlnk_.length - 1];
+    for(var i = 0; i < idlnk_.length; i++) {
+        statesData[i].id = idlnk_[i];
+        idlnk[idlnk_[i]] = i;
+    }
+    // set color
+    for(var i = 0; i < idlnk_.length; i++)
+        statesData[i].color = getColor(i);
+    showAllStates();
+    // set states name
+    var paths = document.querySelectorAll("path");
+    paths = [].slice.call(paths).sort(function(a,b){
+        return a.id.substr(5, 2) - b.id.substr(5, 2);
+    });
+    //[].sort.call( paths, function(a,b) {return a - b;});
+    //paths.sort(function(a, b) {return a - b;});
+    for (var i = 0; i < idlnk_.length; i++)
+        addText(paths[i], statesData[i].name);
+}
+
+function addText(p, text)
+{
+    var t = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    var b = p.getBBox();
+    t.setAttribute("transform", "translate(" + (b.x * width / 1000 + b.width / 2) + " " + (b.y * width / 1000 + b.height / 2) + ")");
+    t.textContent = text;
+    t.setAttribute("fill", "#222");
+    t.setAttribute("font-size", "14");
+    t.setAttribute("font-family", "Verdana");
+    t.setAttribute("font-variant", "small-caps");
+    p.parentNode.insertBefore(t, p.nextSibling);
 }
